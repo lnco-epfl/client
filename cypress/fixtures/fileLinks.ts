d4c54dfe5697245979b645db54c5324499a5a31e
Basile Spaenlehauer <spaenleh@gmail.com>
2024-12-05 11:56:08 +0100

    fix: add https to fixture url (#530)

--- cypress/fixtures/fileLinks.ts
+++ cypress/fixtures/fileLinks.ts
@@ -4,6 +4,6 @@
 
 export const MOCK_IMAGE_URL = 'https://picsum.photos/100';
 export const MOCK_VIDEO_URL =
-  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
 export const MOCK_PDF_URL =
   'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
